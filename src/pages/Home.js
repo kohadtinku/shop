96ef473c3bdbc070af2ac8fd2bc2b9298cc6ba81
tinku kohad <tinkukohad05@gmail.com>
2024-05-20 12:59:37 +0530

    changes-slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000, // Animation duration
-      easing: 'ease-in-out', // Animation easing
+      // easing: 'ease-in-out', // Animation easing
     
     });
   }, []);
