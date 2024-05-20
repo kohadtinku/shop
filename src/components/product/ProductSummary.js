14c58ad0a10f013958fac29ec1bff12fc7d3f9f6
tinku kohad <tinkukohad05@gmail.com>
2024-05-20 17:59:15 +0530

    changes slider

--- src/components/product/ProductSummary.js
+++ src/components/product/ProductSummary.js
@@ -1,54 +1,49 @@
-import React from 'react';
-import reviewsData from '../../data/reviewsData';
-import useActive from '../../hooks/useActive';
-import ProductReviews from './ProductReviews';
-
+import React from "react";
+import reviewsData from "../../data/reviewsData";
+import useActive from "../../hooks/useActive";
+import ProductReviews from "./ProductReviews";
 
 const ProductSummary = (props) => {
+  const { brand, title, info, category, type, connectivity } = props;
 
-    const { brand, title, info, category, type, connectivity } = props;
+  const { active, handleActive, activeClass } = useActive("specs");
 
-    const { active, handleActive, activeClass } = useActive('specs');
+  return (
+    <>
+      <section id="product_summary" className="section">
+        <div className="container">
+          {/*===== Product-Summary-Tabs =====*/}
+          <div className="prod_summary_tabs">
+            <ul className="tabs">
+              <li
+                className={`tabs_item ${activeClass("specs")}`}
+                onClick={() => handleActive("specs")}
+              >
+                Specifications
+              </li>
+              <li
+                className={`tabs_item ${activeClass("overview")}`}
+                onClick={() => handleActive("overview")}
+              >
+                Overview
+              </li>
+              <li
+                className={`tabs_item ${activeClass("reviews")}`}
+                onClick={() => handleActive("reviews")}
+              >
+                Reviews
+              </li>
+            </ul>
+          </div>
 
-
-    return (
-        <>
-            <section id="product_summary" className="section">
-                <div className="container">
-
-                    {/*===== Product-Summary-Tabs =====*/}
-                    <div className="prod_summary_tabs">
-                        <ul className="tabs">
-                            <li
-                                className={`tabs_item ${activeClass('specs')}`}
-                                onClick={() => handleActive('specs')}
-                            >
-                                Specifications
-                            </li>
-                            <li
-                                className={`tabs_item ${activeClass('overview')}`}
-                                onClick={() => handleActive('overview')}
-                            >
-                                Overview
-                            </li>
-                            <li
-                                className={`tabs_item ${activeClass('reviews')}`}
-                                onClick={() => handleActive('reviews')}
-                            >
-                                Reviews
-                            </li>
-                        </ul>
-                    </div>
-
-                    {/*===== Product-Summary-Details =====*/}
-                    <div className="prod_summary_details">
-                        {
-                            active === 'specs' ? (
-                                <div className="prod_specs">
-                                    <ul>
+          {/*===== Product-Summary-Details =====*/}
+          <div className="prod_summary_details">
+            {active === "specs" ? (
+              <div className="prod_specs">
+                {/* <ul>
                                         <li>
                                             <span>Brand</span>
-                                            <span>{brand}</span>
+                                            <span className='custom-span' style={{textAlign:"start"}}>{brand}</span>
                                         </li>
                                         <li>
                                             <span>Model</span>
@@ -70,41 +65,78 @@ const ProductSummary = (props) => {
                                             <span>Microphone</span>
                                             <span>Yes</span>
                                         </li>
-                                    </ul>
-                                </div>
-                            ) : active === 'overview' ? (
-                                <div className="prod_overview">
-                                    <h3>The <span>{title}</span> {info} provides with fabulous sound quality</h3>
-                                    <ul>
-                                        <li>Sound Tuned to Perfection</li>
-                                        <li>Comfortable to Wear</li>
-                                        <li>Long Hours Playback Time</li>
-                                    </ul>
-                                    <p>Buy the <b>{title} {info}</b> which offers you with fabulous music experience by providing you with awesome sound quality that you can never move on from. Enjoy perfect flexibility and mobility with amazing musical quality with these {category} giving you a truly awesome audio experience. It blends with exceptional sound quality and a range of smart features for an unrivalled listening experience.</p>
-                                </div>
-                            ) : (
-                                <div className="prod_reviews">
-                                    <ul>
-                                        {
-                                            reviewsData.map(item => (
-                                                <ProductReviews
-                                                    key={item.id}
-                                                    {...item}
-                                                />
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                            )
+                                    </ul> */}
 
-                        }
-
-                    </div>
-
-                </div>
-            </section>
-        </>
-    );
+                <table className="product-details-table">
+                  <tbody>
+                    <tr>
+                      <td>Brand</td>
+                      <td className="custom-td">
+                        {brand}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Model</td>
+                      <td>{title}</td>
+                    </tr>
+                    <tr>
+                      <td>Generic Name</td>
+                      <td>{category}</td>
+                    </tr>
+                    <tr>
+                      <td>Headphone Type</td>
+                      <td>{type}</td>
+                    </tr>
+                    <tr>
+                      <td>Connectivity</td>
+                      <td>{connectivity}</td>
+                    </tr>
+                    <tr>
+                      <td>Microphone</td>
+                      <td>Yes</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            ) : active === "overview" ? (
+              <div className="prod_overview">
+                <h3>
+                  The <span>{title}</span> {info} provides with fabulous sound
+                  quality
+                </h3>
+                <ul>
+                  <li>Sound Tuned to Perfection</li>
+                  <li>Comfortable to Wear</li>
+                  <li>Long Hours Playback Time</li>
+                </ul>
+                <p>
+                  Buy the{" "}
+                  <b>
+                    {title} {info}
+                  </b>{" "}
+                  which offers you with fabulous music experience by providing
+                  you with awesome sound quality that you can never move on
+                  from. Enjoy perfect flexibility and mobility with amazing
+                  musical quality with these {category} giving you a truly
+                  awesome audio experience. It blends with exceptional sound
+                  quality and a range of smart features for an unrivalled
+                  listening experience.
+                </p>
+              </div>
+            ) : (
+              <div className="prod_reviews">
+                <ul>
+                  {reviewsData.map((item) => (
+                    <ProductReviews key={item.id} {...item} />
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductSummary;
