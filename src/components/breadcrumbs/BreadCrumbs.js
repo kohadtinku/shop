250e8d44128c1fa045727aac11de4ec14ee298dd
tinku kohad <tinkukohad05@gmail.com>
2024-07-12 15:20:39 +0530

    cart items added in localstorage

--- src/components/breadcrumbs/BreadCrumbs.js
+++ src/components/breadcrumbs/BreadCrumbs.js
@@ -1,37 +1,3 @@
-// import React from 'react';
-// import { Link, useLocation } from 'react-router-dom';
-// import './breadcrumbs.css';
-
-// const BreadCrumbs = ({ crumbs }) => {
-//   const location = useLocation();
-//   const currentPath = location.pathname;
-
-//   const filteredCrumbs = crumbs.filter(crumb => {
-//     const crumbPath = crumb.to.replace(':productId', '').replace(':searchTerm', '');
-//     return currentPath.startsWith(crumbPath);
-//   });
-
-//   return (
-//     <nav aria-label="Breadcrumb">
-//       <ol className="breadcrumb">
-//         {filteredCrumbs.map((crumb, index) => (
-//           <li key={index} className="breadcrumb-item">
-//             {index === filteredCrumbs.length - 1 ? (
-//               <span>{crumb.label}</span>
-//             ) : (
-//               <Link to={crumb.to}>{crumb.label}</Link>
-//             )}
-//           </li>
-//         ))}
-//       </ol>
-//     </nav>
-//   );
-// };
-
-// export default BreadCrumbs;
-
-
-
 
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
