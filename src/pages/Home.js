39dfe23a7c4dfbd112b68eca1ead9031f49885a8
tinku kohad <tinkukohad05@gmail.com>
2024-07-12 14:11:22 +0530

    slider add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,57 +13,68 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Home = () => {
-
   useEffect(() => {
     AOS.init({
       duration: 2000, // Animation duration
       // easing: 'ease-in-out', // Animation easing
-    
     });
   }, []);
   return (
-    <main>
-      <section
-        className="topbarproducts"
-        // style={{
-        //   marginTop: "100px",
-        // }}
-      >
-      {/* <MainSlider/> */}
-        <div style={{display:"flex",
-        justifyContent:"center",alignItems:"center","flexDirection":"column",gap:"10px",marginTop:"100px"}} data-aos="fade-down">
-          <h1>Categories</h1>
-          {/* <TopBarProducts/> */}
-          <TopSlider />
-        </div>
-      </section>
-      <section id="products" className="section">
-        <div className="container" data-aos="zoom-in">
-          {/* <SectionsHead heading="Top Products" /> */}
-          {/* <TopProducts /> */}
-          <OurProducts />
-        </div>
-      </section>
-      <section id="hero" data-aos="fade-down">
-        <HeroSlider />
-      </section>
+    <>
+      <main>
+        <section
+          className="topbarproducts"
+          // style={{
+          //   marginTop: "100px",
+          // }}
+        >
+          {/* <MainSlider/> */}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              gap: "10px",
+              marginTop: "100px",
+            }}
+            data-aos="fade-down"
+          >
+            <h1>Categories</h1>
+            {/* <TopBarProducts/> */}
+            <TopSlider />
+          </div>
+        </section>
+        <section id="products" className="section">
+          <div className="container" data-aos="zoom-in">
+            {/* <SectionsHead heading="Top Products" /> */}
+            {/* <TopProducts /> */}
+            <OurProducts />
+          </div>
+        </section>
+        <section id="hero" data-aos="fade-down">
+          <HeroSlider />
+        </section>
 
-      <section id="featured" className="section">
-        <div className="container" data-aos="zoom-in">
-          <SectionsHead heading="Featured Products" />
-          <FeaturedSlider  />
-        </div>
-      </section>
+        <section id="featured" className="section">
+          <div className="container" data-aos="zoom-in">
+            <SectionsHead heading="Featured Products" />
+            <FeaturedSlider />
+          </div>
+        </section>
 
-      {/* <section id="featured" className="section">
+        {/* <section id="featured" className="section">
                 <div className="container">
                     <SectionsHead heading="Today Best Deals" />
                     <SwipperSlider />
                 </div>
             </section> */}
 
-      <Services />
-    </main>
+        <Services />
+      </main>
+
+    
+    </>
   );
 };
 
